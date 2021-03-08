67437bc10b454b49887b1b19c0c98691275d832f
Felipe Garcia Alves <47622464+felipAlves@users.noreply.github.com>
2021-03-08 19:46:45 -0300

    Template inicial

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ThemeContainer from '../contexts/theme/ThemeContainer'
+
+import { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <ThemeContainer>
+      <Component {...pageProps} />
+    </ThemeContainer>
+  )
+}
+
+export default MyApp
